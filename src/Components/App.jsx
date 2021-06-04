6a51df1b20419fa89853d21e4bf152ee79932f6b
ebrahim allawi <ebrahimallawi4@gmail.com>
2021-06-04 13:13:42 +0300

    init

---
+++ src/Components/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect, useRef } from "react";
+// import "../SCSS/App.scss";
+import Note from "./Note";
+// 
+const App = () => {
+
+  return (
+    <>
+      
+    </>
+  );
+};
+
+export default App;
