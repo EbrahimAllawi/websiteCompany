f0f04639d32391f85af343f882ba7d5cec23efa8
ebrahim allawi <ebrahimallawi4@gmail.com>
2021-06-04 13:21:05 +0300

    PWA

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,12 +1,10 @@
 import React, { useState, useEffect, useRef } from "react";
-// import "../SCSS/App.scss";
-import Note from "./Note";
-// 
+
 const App = () => {
 
   return (
     <>
-      
+      a
     </>
   );
 };
